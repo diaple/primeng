9776cf384a2a03926690ab64842edb7ff87fad4e
Luis Acevedo Mur <luis.acevedo@diaple.com>
2021-04-28 11:04:18 +0200

    -

--- src/app/components/calendar/calendar.ts
+++ src/app/components/calendar/calendar.ts
@@ -2129,12 +2129,14 @@ export class Calendar implements OnInit,OnDestroy,ControlValueAccessor {
 		}
     }
 
-    writeValue(value: any) : void {
+    writeValue(value) {
         this.value = value;
+        if(typeof this.value === 'string'){
+            this.value = new Date(this.value);
+        }
         if (this.value && typeof this.value === 'string') {
             this.value = this.parseValueFromString(this.value);
         }
-
         this.updateInputfield();
         this.updateUI();
         this.cd.markForCheck();
